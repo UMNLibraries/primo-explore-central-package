6be402042fef857b268681aa4146616aab85d844
gpeterso <gpeterso@umn.edu>
2016-12-08 11:27:51 -0600

    Update HathiTrust availability package

--- js/custom.js
+++ js/custom.js
@@ -81,7 +81,56 @@ app.component('pubmedFix', {
   controller: 'pubmedFixController'
 });
 
-var app = app || angular.module('viewCustom', []);
+app.component('hathiTrustAvailability', {
+  require: {
+    parent: '^prmSearchResultAvailabilityLineAfter'
+  },
+  bindings: {
+    hideOnline: '<',
+    msg: '@?'
+  },
+  controller: 'hathiTrustAvailabilityController',
+  template: '<span class="umnHathiTrustLink">\
+              <a target="_blank" ng-if="$ctrl.fullTextLink" ng-href="{{$ctrl.fullTextLink}}">\
+              {{$ctrl.msg}}\
+                <prm-icon external-link="" icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new"></prm-icon>\
+              </a>\
+            </span>'
+
+});
+
+app.controller('hathiTrustAvailabilityController', ['hathiTrust', function (hathiTrust) {
+  var self = this;
+
+  self.$onInit = function () {
+    self.parentCtrl = this.parent.parentCtrl;
+    setDefaults();
+    if (!(isOnline() && self.hideOnline)) {
+      updateHathiTrustAvailability();
+    }
+  };
+
+  var setDefaults = function setDefaults() {
+    if (!self.msg) self.msg = 'Full Text Available at HathiTrust';
+  };
+
+  var isOnline = function isOnline() {
+    return self.parentCtrl.result.delivery.GetIt1.some(function (g) {
+      return g.links.some(function (l) {
+        return l.isLinktoOnline;
+      });
+    });
+  };
+
+  var updateHathiTrustAvailability = function updateHathiTrustAvailability() {
+    var hathiTrustIds = (self.parentCtrl.result.pnx.addata.oclcid || []).map(function (id) {
+      return "oclc:" + id;
+    });
+    hathiTrust.findFullViewRecord(hathiTrustIds).then(function (res) {
+      self.fullTextLink = res;
+    });
+  };
+}]);
 
 app.factory('hathiTrust', ['$http', '$q', function ($http, $q) {
   var svc = {};
@@ -112,47 +161,4 @@ app.factory('hathiTrust', ['$http', '$q', function ($http, $q) {
   };
   return svc;
 }]);
-
-app.controller('hathiTrustAvailabilityController', ['hathiTrust', function (hathiTrust) {
-  var self = this;
-
-  self.$onInit = function () {
-    self.parentCtrl = this.parent.parentCtrl;
-    if (!(isOnline() && self.hideOnline)) {
-      updateHathiTrustAvailability();
-    }
-  };
-
-  var isOnline = function isOnline() {
-    return self.parentCtrl.result.delivery.GetIt1.some(function (g) {
-      return g.links.some(function (l) {
-        return l.isLinktoOnline;
-      });
-    });
-  };
-
-  var updateHathiTrustAvailability = function updateHathiTrustAvailability() {
-    var hathiTrustIds = (self.parentCtrl.result.pnx.addata.oclcid || []).map(function (id) {
-      return "oclc:" + id;
-    });
-    hathiTrust.findFullViewRecord(hathiTrustIds).then(function (res) {
-      self.fullTextLink = res;
-    });
-  };
-}]);
-
-app.component('hathiTrustAvailability', {
-  require: {
-    parent: '^prmSearchResultAvailabilityLineAfter'
-  },
-  bindings: { hideOnline: '<' },
-  controller: 'hathiTrustAvailabilityController',
-  template: '<span class="umnHathiTrustLink">\
-              <a target="_blank" ng-if="$ctrl.fullTextLink" ng-href="{{$ctrl.fullTextLink}}">\
-                Full Text Available at HathiTrust\
-                <prm-icon external-link="" icon-type="svg" svg-icon-set="primo-ui" icon-definition="open-in-new"></prm-icon>\
-              </a>\
-            </span>'
-
-});
 })();
