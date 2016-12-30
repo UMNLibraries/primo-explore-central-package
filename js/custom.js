4ea6471f618f0107c778702c9f259f176495d9b5
gpeterso <gpeterso@umn.edu>
2016-12-30 14:14:35 -0600

    Refactor GA code
    Moving GA stuff into a service

--- js/custom.js
+++ js/custom.js
@@ -45,35 +45,55 @@ app.run(['bloggerNotifications', function (bloggerNotifications) {
   bloggerNotifications.show(bloggerUrl);
 }]);
 
-app.run(['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
-  (function (i, s, o, g, r, a, m) {
-    i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
-      (i[r].q = i[r].q || []).push(arguments);
-    }, i[r].l = 1 * new Date();a = s.createElement(o), m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
-  })($window, $window.document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+app.run(['googleAnalytics', function (googleAnalytics) {
 
-  var isProd = /^(primo.lib|prime2.oit|umn-almaprimo.hosted).*/.test($location.host());
+  googleAnalytics.initialize();
+  googleAnalytics.trackPageviews();
+}]);
 
-  var trackingId;
+app.factory('googleAnalytics', ['$rootScope', '$location', '$window', function ($rootScope, $location, $window) {
 
-  if (isProd) {
-    trackingId = 'UA-20973358-32';
-  } else {
-    trackingId = 'UA-20973358-29';
-  }
+  var svc = {};
 
-  $window.ga('create', trackingId, 'auto');
-  $window.ga('set', 'anonymizeIp', true);
+  var getTrackingId = function getTrackingId() {
+    var isProd = /^(primo.lib|prime2.oit|umn-almaprimo.hosted).*/.test($location.host());
+    if (isProd) {
+      return 'UA-20973358-32';
+    } else {
+      return 'UA-20973358-29';
+    }
+  };
 
-  var view = $location.search().vid || '';
-  if (/^(twincities|duluth|morris|crookston)(_sp)?$/i.test(view)) {
-    var campus = view.toLowerCase().replace(/_sp$/, '');
-    $window.ga('set', 'dimension1', campus);
-  }
+  var setCampusDim = function setCampusDim() {
+    var view = $location.search().vid || '';
+    if (/^(twincities|duluth|morris|crookston)(_sp)?$/i.test(view)) {
+      var campus = view.toLowerCase().replace(/_sp$/, '');
+      $window.ga('set', 'dimension1', campus);
+    }
+  };
 
-  $rootScope.$on('$locationChangeSuccess', function (event) {
-    $window.ga('send', 'pageview', { location: $location.url() });
-  });
+  var loadGa = function loadGa() {
+    (function (i, s, o, g, r, a, m) {
+      i['GoogleAnalyticsObject'] = r;i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments);
+      }, i[r].l = 1 * new Date();a = s.createElement(o), m = s.getElementsByTagName(o)[0];a.async = 1;a.src = g;m.parentNode.insertBefore(a, m);
+    })($window, $window.document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+  };
+
+  svc.initialize = function () {
+    loadGa();
+    $window.ga('create', getTrackingId(), 'auto');
+    $window.ga('set', 'anonymizeIp', true);
+    setCampusDim();
+  };
+
+  svc.trackPageviews = function () {
+    $rootScope.$on('$locationChangeSuccess', function (event) {
+      $window.ga('send', 'pageview', { location: $location.url() });
+    });
+  };
+
+  return svc;
 }]);
 
 app.component('prmSearchResultAvailabilityLineAfter', {
--- js/ga.js
+++ js/ga.js
@@ -1,30 +1,53 @@
-app.run(['$rootScope', '$location', '$window', function($rootScope, $location, $window){
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })($window,$window.document,'script','https://www.google-analytics.com/analytics.js','ga');
+app.run(['googleAnalytics', function(googleAnalytics){
 
-  var isProd = /^(primo.lib|prime2.oit|umn-almaprimo.hosted).*/.test($location.host());
-
-  var trackingId;
-
-  if (isProd) {
-    trackingId = 'UA-20973358-32';
-  } else {
-    trackingId = 'UA-20973358-29';
-  }
-
-  $window.ga('create', trackingId, 'auto');
-  $window.ga('set', 'anonymizeIp', true);
-
-  var view = ($location.search().vid || '');
-  if (/^(twincities|duluth|morris|crookston)(_sp)?$/i.test(view)) {
-    var campus = view.toLowerCase().replace(/_sp$/, '');
-    $window.ga('set', 'dimension1', campus);
-  }
-
-  $rootScope.$on('$locationChangeSuccess', function(event){
-    $window.ga('send', 'pageview', {location: $location.url()});
-  });
+  googleAnalytics.initialize();
+  googleAnalytics.trackPageviews();
 
 }]);
+
+app.factory('googleAnalytics', ['$rootScope', '$location', '$window', function($rootScope, $location, $window) {
+
+  var svc = {}; 
+
+  var getTrackingId = function() {
+    var isProd = /^(primo.lib|prime2.oit|umn-almaprimo.hosted).*/.test($location.host());
+    if (isProd) {
+      return 'UA-20973358-32';
+    } else {
+      return 'UA-20973358-29';
+    }
+  }
+
+  var setCampusDim = function() {
+    var view = ($location.search().vid || '');
+    if (/^(twincities|duluth|morris|crookston)(_sp)?$/i.test(view)) {
+      var campus = view.toLowerCase().replace(/_sp$/, '');
+      $window.ga('set', 'dimension1', campus);
+    }
+  }
+
+  var loadGa = function() {
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })($window,$window.document,'script','https://www.google-analytics.com/analytics.js','ga');
+  }
+
+  svc.initialize = function() {
+    loadGa(); 
+    $window.ga('create', getTrackingId(), 'auto');
+    $window.ga('set', 'anonymizeIp', true);
+    setCampusDim();
+  }
+
+  svc.trackPageviews = function() {
+    $rootScope.$on('$locationChangeSuccess', function(event){
+      $window.ga('send', 'pageview', {location: $location.url()});
+    });
+  }
+
+  return svc;
+
+}]);
+
+
