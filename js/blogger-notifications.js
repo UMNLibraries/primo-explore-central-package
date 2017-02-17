1cc0cef25606dd88e2a512419c3d38c2d76dd22c
gpeterso <gpeterso@umn.edu>
2017-02-17 11:54:15 -0600

    Add es2015 blogger notifications

--- js/blogger-notifications.js
+++ js/blogger-notifications.js
@@ -1,3 +1,45 @@
+export class BloggerNotifications {
+  constructor($mdToast, $http) {
+    this.$mdToast = $mdToast;
+    this.$http = $http;
+  }
+
+  showToast(msg) {
+      this.$mdToast.show({
+        controllerAs: 'ctrl',
+        bindToController: true,
+        autoWrap: false,
+        position: 'start',
+        hideDelay: 0,
+        controller: ['$mdToast', function($mdToast) {
+          this.msg = msg;
+          this.close = function() {$mdToast.hide()};
+        }],       
+        template: `<md-toast class="blogger-notification">
+                    <span class="md-toast-text" flex>{{ctrl.msg}}</span>
+                    <md-button ng-click="ctrl.close()">Close<md-button>
+                  </md-toast>`
+                  
+      }); 
+  }
+
+  show(url) {
+      var url = url + '?alt=json-in-script&callback=JSON_CALLBACK';
+      this.$http.jsonp(url).success(data => {
+      var entry = data.feed.entry;
+      if (entry) {
+        let title = entry[0].title.$t;
+        let content = entry[0].content.$t;
+        this.showToast(title + ': ' + content);
+      }  
+    });
+  }  
+
+}
+
+BloggerNotifications.$inject = ['$mdToast', '$http'];
+
+/*
 app.factory('bloggerNotifications', ['$mdToast', '$http', '$timeout', function($mdToast, $http, $timeout) {
   var svc = {};
 
@@ -40,3 +82,5 @@ app.run(['bloggerNotifications', function(bloggerNotifications) {
   var bloggerUrl = 'https://umnprimonotifications.blogspot.com/feeds/posts/default';
   bloggerNotifications.show(bloggerUrl);
 }]);
+*/
+
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {PubmedLinkFix} from './pubmeb-fix'
+import {BloggerNotifications} from './blogger-notifications.js'
 
 let app = angular.module('centralCustom', ['angularLoad']);
 
@@ -6,4 +7,13 @@ app.component('pubmedLinkFix', PubmedLinkFix);
 app.component('prmServiceLinksAfter', {
   template: '<pubmed-link-fix></pubmed-link-fix>'
 });
+
+app.service('BloggerNotifications', BloggerNotifications);
+
+
+app.run(['BloggerNotifications', function(bloggerNotifications) {
+  var bloggerUrl = 'https://umnprimonotifications.blogspot.com/feeds/posts/default';
+  console.log(bloggerNotifications);
+  bloggerNotifications.show(bloggerUrl);
+}]);
 
