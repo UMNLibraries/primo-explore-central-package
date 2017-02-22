e6c7d762fcf77d281bd4d8496abf68816d99d078
gpeterso <gpeterso@umn.edu>
2017-02-22 15:18:27 -0600

    Cleanup

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
+import 'primo-explore-hathitrust-availability'
 import './google-analytics'
 import './blogger-notifications'
 import PubmedLinkFix from './pubmeb-fix'
 
+const REQUIRED_MODULES = [ 'angularLoad', 
+                           'hathiTrustAvailability', 
+                           'googleAnalytics', 
+                           'bloggerNotifications' ];
+
 angular
-  .module('centralCustom', ['angularLoad', 'googleAnalytics', 'bloggerNotifications'])
+  .module('centralCustom', REQUIRED_MODULES)
+  
   .component('pubmedLinkFix', PubmedLinkFix)
   .component('prmServiceLinksAfter', {
     template: '<pubmed-link-fix></pubmed-link-fix>'
+  })
+
+  .component('prmSearchResultAvailabilityLineAfter', { 
+    template: '<hathi-trust-availability hide-online=true></hathi-trust-availability>'
   });
 
 
