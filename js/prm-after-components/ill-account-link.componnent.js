186f146e6e787e5ca1e1b8ba00e3b37a55a5391b
gpeterso <gpeterso@umn.edu>
2017-05-31 10:44:10 -0500

    Add ILL account link

---
+++ js/prm-after-components/ill-account-link.componnent.js
@@ -0,0 +1,28 @@
+class IllAccountLinkController {
+  constructor($window) {
+    this._$window = $window;
+  }
+  $onInit() {
+    this.inst = this._$window.appConfig['primo-view']['institution']['institution-code'];
+  }
+  get link() {
+    if (this.inst === 'TWINCITIES') {
+      return 'http://ezproxy.lib.umn.edu/login?qurl=https%3A%2F%2Fumn.illiad.oclc.org%2Filliad%2FILLiad.dll';
+    } else
+      return null;
+  }
+}
+
+IllAccountLinkController.$inject = ['$window'];
+
+export default {
+  controller: IllAccountLinkController,
+  template: `
+            <md-button class="md-raised md-primary" ng-if="$ctrl.link" ng-href="{{$ctrl.link}}">
+            <md-tooltip>Sign in to you interlibrary loan accout</md-tooltip>
+              My ILL Account
+            </md-button>
+            `
+};
+
+
--- js/prm-after-components/prm-after.module.js
+++ js/prm-after-components/prm-after.module.js
@@ -2,6 +2,7 @@ import HathiTrustAvailability from 'primo-explore-hathitrust-availability'
 import RemoveAlmaSkin from './remove-alma-skin.component'
 import PubmedLinkFix from './pubmeb-fix.component'
 import HidePcAvailability from './hide-pc-availability.component'
+import IllAccountLink from './ill-account-link.componnent'
 
 export default angular
   .module('prmAfterComponents', [
@@ -10,6 +11,7 @@ export default angular
   .component('pubmedLinkFix', PubmedLinkFix)
   .component('hidePcAvailability', HidePcAvailability)
   .component('removeAlmaSkin', RemoveAlmaSkin)
+  .component('illAccountLink', IllAccountLink)
   .component('prmServiceLinksAfter', {
     template: '<pubmed-link-fix></pubmed-link-fix>'
   })
@@ -21,6 +23,9 @@ export default angular
   })
   .component('prmFullViewServiceContainerAfter', {
     template: '<remove-alma-skin></remove-alma-skin>'
+  })
+  .component('prmLinkedUserSelectorAfter', {
+    template: '<ill-account-link></ill-account-link>'
   })
   .name;
 
