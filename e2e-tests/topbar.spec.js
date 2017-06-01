6460cdda810bc67479c471a36273b30ef3637836
gpeterso <gpeterso@umn.edu>
2017-06-01 13:39:44 -0500

    Remove "tags" hiding from css
    The tags link can be removed from the main menu in the views
    wizard, which is arguably a better approach.

--- e2e-tests/topbar.spec.js
+++ e2e-tests/topbar.spec.js
@@ -1,9 +1,13 @@
 describe('Top Bar', () => {
   describe('Tags link', () => {
-    it('should be hidden', () => {
+    it('should not be present', () => {
+      /*
+       * Removing the tags link is done in the 
+       * Back Office "views wizard"
+       */
       browser.get('search?vid=TWINCITIES&lang=en_US');
       expect(
-        element(by.css('prm-main-menu a[aria-label="nui.mainmenu.label.tags"]')).isDisplayed()
+        element(by.css('prm-main-menu a[aria-label="nui.mainmenu.label.tags"]')).isPresent()
       ).toBeFalsy();
     });
   });
