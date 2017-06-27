3ecb3cb124482aec722176715a96075ff109c3c7
gpeterso <gpeterso@umn.edu>
2017-06-27 15:16:42 -0500

    Fix typo in "haven't found" card

--- js/prm-after-components/prm-search-result-list-after.component.js
+++ js/prm-after-components/prm-search-result-list-after.component.js
@@ -32,7 +32,7 @@ export default {
       </md-card-title>
       <md-card-content>
         <ul>
-          <li>Use our <a href='https://umn.illiad.oclc.org/illiad/logon.html'> interlibrary loan service</a> to request it from annother library.</li>
+          <li>Use our <a href='https://umn.illiad.oclc.org/illiad/logon.html'> interlibrary loan service</a> to request it from another library.</li>
           <li>Try searching <a href='http://www.worldcat.org/search?q={{$ctrl.searchString}}'>WorldCat®</a>, <a href='http://www.lib.umn.edu/slog.phtml?url=http://scholar.google.com/scholar?q={{$ctrl.searchString}}'>Google™ Scholar</a>, or the <a href='https://lib.umn.edu'>Libraries Home Page</a>.</li> 
           <li><a href='https://www.lib.umn.edu/help'>Ask us</a> for assistance.</li>
         </ul>
