80334dd795702cea630a3058d921c98cf7bbbf8c
gpeterso <gpeterso@umn.edu>
2017-06-28 12:08:30 -0500

    Update blogger notifications
    - increase timeout to 20 seconds
    - add a dismiss button
    - remember dismissal for 1 day (via cookie)

--- js/blogger-notifications/blogger-notifications.service.js
+++ js/blogger-notifications/blogger-notifications.service.js
@@ -1,9 +1,10 @@
 class BloggerNotifications {
 
-  constructor($mdToast, $http, $document, bloggerBaseUrl) {
+  constructor($mdToast, $http, $document, bloggerBaseUrl, $cookies) {
     this._$mdToast = $mdToast;
     this._$http = $http;
     this._$document = $document;
+    this._$cookies = $cookies;
     this.url = `${bloggerBaseUrl}/feeds/posts/default?alt=json-in-script`;
   }
 
@@ -14,22 +15,38 @@ class BloggerNotifications {
       autoWrap: false,
       position: 'top',
       parent: this._$document[0].getElementsByTagName('html')[0],
-      hideDelay: 10000,
+      hideDelay: 20000,
       locals: {
         $mdToast: this._$mdToast, 
+        notificationService: this,
         msg: msg
       },
       controller: class {
         close() {
           this.$mdToast.hide()
+          notificationService._markNotificationAsRead();
         };
       },       
       template: `<md-toast class="blogger-notification">
                   <span class="md-toast-text" flex>{{ctrl.msg}}</span>
-                  <!--<md-button ng-click="ctrl.close()">Close<md-button>-->
+                  <md-button class='md-icon-button' aria-label='Dismiss notification' ng-click="ctrl.close()">
+                    <md-icon md-svg-icon="navigation:ic_close_24px"><span>Dismiss</span></md-icon>
+                  <md-button>
                 </md-toast>`
                 
     }); 
+  }
+
+  _markNotificationAsRead() {
+    let cookieName = `unmnotification_${this.notificationTimestamp}`;
+    let expireDate = new Date();
+    expireDate.setDate(expireDate.getDate() + 1);
+    this._$cookies.put(cookieName, '1', {expires: expireDate});
+  }
+
+  _notificationIsUnread() {
+    let cookieName = `unmnotification_${this.notificationTimestamp}`;
+    return (this._$cookies.get(cookieName) !== '1');
   }
 
   show() {
@@ -39,14 +56,17 @@ class BloggerNotifications {
       if (entry) {
         let title = entry[0].title.$t;
         let content = entry[0].content.$t;
-        this._showToast(title + ': ' + content);
+        this.notificationTimestamp = entry[0].updated.$t;
+        if (this._notificationIsUnread()) {
+          this._showToast(title + ': ' + content);
+        }
       }  
     });
   }  
 
 }
 
-BloggerNotifications.$inject = ['$mdToast', '$http', '$document', 'bloggerBaseUrl'];
+BloggerNotifications.$inject = ['$mdToast', '$http', '$document', 'bloggerBaseUrl', '$cookies'];
 
 export default BloggerNotifications;
 
