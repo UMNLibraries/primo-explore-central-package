3874cc831aab87dbc3ff2769e32675fc6ab2f02d
gpeterso <gpeterso@umn.edu>
2017-07-06 15:11:29 -0500

    Add tests for GA service

---
+++ js/google-analytics/google-analytics.service.spec.js
@@ -0,0 +1,39 @@
+
+describe('GoogleAnalytics Service', () => {
+
+  let $rootScope, $location, $window;
+
+  beforeEach(angular.mock.module('googleAnalytics'));
+
+  beforeEach(inject($injector => {
+    $rootScope = $injector.get('$rootScope');
+    $location = $injector.get('$location');
+    $window = $injector.get('$window');
+    googleAnalyticsService = $injector.get('googleAnalytics');
+  }));
+
+  it('should create a global ga function', () => {
+    expect($window.ga).toBeDefined();
+  });
+
+  it('should have a campus dimension based on the view id', () => {
+    $location.url('/primo-explore/search?vid=TWINCITIES');
+    expect(googleAnalyticsService.campusDim).toBe('twincities');
+    $location.url('/primo-explore/search?vid=DULUTH');
+    expect(googleAnalyticsService.campusDim).toBe('duluth');
+    $location.url('/primo-explore/search?vid=MORRIS');
+    expect(googleAnalyticsService.campusDim).toBe('morris');
+    $location.url('/primo-explore/search?vid=CROOKSTON');
+    expect(googleAnalyticsService.campusDim).toBe('crookston');
+  });
+
+  it('should record pageview events when the location changes', () => {
+    let url = "/foo";
+    $location.url(url);
+    spyOn($window, 'ga');
+    googleAnalyticsService.trackPageviews();
+    $rootScope.$broadcast('$locationChangeSuccess');
+    expect($window.ga).toHaveBeenCalledWith('send', 'pageview', {location: url});
+  });
+
+});
