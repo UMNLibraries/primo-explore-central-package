758926ace551470f814d13f87505ed74fc9aafb8
gpeterso <gpeterso@umn.edu>
2017-07-19 14:41:25 -0500

    Add e2e test for the search bar

---
+++ e2e-tests/searchbar.spec.js
@@ -0,0 +1,13 @@
+describe('Search Bar', () => {
+  describe('Autocomplete', () => {
+    it('should not display tab suggestions in the TWINCITIES view', () => {
+      browser.get('search?vid=TWINCITIES');
+      let searchBar = element(by.id('searchBar'));
+      let autoCompleteContainer = element(by.id('prm-simple-search'));
+      searchBar.sendKeys('test');
+      expect(autoCompleteContainer.getText()).not.toContain('MNCAT Discovery');
+      expect(autoCompleteContainer.getText()).not.toContain('Libraries Catalog');
+    });
+  });
+
+});
