6be3900b8fa5e87c33d127c75660914ed71cebeb
gpeterso <gpeterso@umn.edu>
2017-07-26 16:36:46 -0500

    Update redirect service
    Seems more stable now...

--- js/redirect-service/redirect.service.js
+++ js/redirect-service/redirect.service.js
@@ -1,18 +1,26 @@
+/**
+ * This service attempts to send the user to the "My Library Card" page
+ * when the URL contains a "&redirect=myaccount" parameter. This is an 
+ * attempt to get around the fact that it's *really* hard to provide 
+ * consistent deep links to this page. The default behavior can vary
+ * depending on whether the user has a new, exiting, or expired session.
+ */
+
 class Redirect {
-  constructor($location, $window, $state, SessionTimeoutService) {
+  constructor($location, $window, $state, SessionTimeoutService, $timeout) {
     this.$location = $location; 
     this.$state = $state;
     this.$window = $window;
+    this.$timeout = $timeout;
     this.SessionTimeoutService = SessionTimeoutService;
   }
 
   init() {
     if (this.redirectRequested()) {
       if (this.isSessionExpired()) {
-        this.reloadPage();
-      } else {
-        this.goToMyLibraryCard();
+        this.SessionTimeoutService.startSession();
       }
+      this.goToMyLibraryCard();
     }
   }
 
@@ -29,14 +37,19 @@ class Redirect {
   }
 
   goToMyLibraryCard() {
-    this.$state.go('account', {vid: this.vid, lang: this.lang, section: 'overview'}, {reload: true});
+    console.log("redirecting to account");
+    // This is pretty ugly, but it only seems to work consistently if $timeout
+    // is used to wait for the next digest cycle. 
+    this.$timeout(() => {
+      this.$state.go('account', {vid: this.vid, lang: this.lang, section: 'overview'}, {reload: true});
+    });
   }
 
   reloadPage() {
-    this.$state.reload();
+    this.$window.location.reload();
   }
 }
 
-Redirect.$inject = ['$location', '$window', '$state', 'SessionTimeoutService'];
+Redirect.$inject = ['$location', '$window', '$state', 'SessionTimeoutService', '$timeout'];
 
 export default Redirect;
