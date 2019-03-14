6c4036d5c0e29e12688df5f944fdef6dbad6fbb0
gpeterso <gpeterso@umn.edu>
2019-03-14 08:35:25 -0500

    Remove prmAccountOverviewAfter component
    This was serving no purpose. It was likely left over from the
    last time the account overview components were updated.

--- js/components/account/overview/account-overview.module.js
+++ js/components/account/overview/account-overview.module.js
@@ -1,11 +1,9 @@
 import CustomTiles from './custom-tiles/custom-tiles.module';
 import PrmLoansOverviewAfter from './loans-overview/prm-loans-overview-after.component';
-import PrmAccountOverviewAfter from './prm-account-overview-after.component';
 
 export default angular
   .module('accountOverview', [
     CustomTiles
   ]) 
   .component('prmLoansOverviewAfter', PrmLoansOverviewAfter)
-  .component('prmAccountOverviewAfter', PrmAccountOverviewAfter)
   .name;
