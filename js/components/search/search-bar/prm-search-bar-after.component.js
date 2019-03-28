4fd4fe9d5f340886a4c8f3641be5a13fe997e2d3
gpeterso <gpeterso@umn.edu>
2019-03-28 12:35:32 -0500

    Hide blended tab selector
    Hide the tab selector in the basic search *if* TC article_discovery
    tab is selected.
    
    NOTE: If we want to hide the tab selector in all cases, regardless
    of which tab/scope we're on, then no custom JS is needed. We can
    just use CSS: `div.search-options { display: none !important; }`

--- js/components/search/search-bar/prm-search-bar-after.component.js
+++ js/components/search/search-bar/prm-search-bar-after.component.js
@@ -1,10 +1,44 @@
 class PrmSearchBarAfterController {
+
+  constructor($scope, $element) {
+    this.$scope = $scope;
+    this.$element = $element;
+  }
+  
   $onInit() {
-    if (this.parentCtrl.vid === 'TWINCITIES' || this.parentCtrl.vid === 'MORRIS') {
-      this.parentCtrl.tabs = [];
+    switch (this.parentCtrl.vid) {
+    case 'TWINCITIES': 
+      this.hideTabSuggestions();
+      this.hideBlendedTabSelector();
+      // The parent controller re-initializes its settings on search state changes, 
+      // so we need to re-hide the selector 
+      this.$scope.$on('$stateChangeSuccess', () => this.hideBlendedTabSelector());
+      break;
+    case 'MORRIS': 
+      this.hideTabSuggestions();
+      break;
     }
   }
+
+  hideTabSuggestions() {
+    this.parentCtrl.tabs = [];
+  }
+
+  hideBlendedTabSelector() {
+    if (this.onBlendedTab()) this.hideTabSelector();
+  }
+
+  onBlendedTab() {
+    return this.parentCtrl.selectedTab === 'article_discovery';
+  }
+
+  hideTabSelector() {
+    this.parentCtrl.showTabsAndScopes = false;
+  }
+
 }
+
+PrmSearchBarAfterController.$inject = ['$scope', '$element'];
 
 export default {
   bindings: {parentCtrl: '<'},
