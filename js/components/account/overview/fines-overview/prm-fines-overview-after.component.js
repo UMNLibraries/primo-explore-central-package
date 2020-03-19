fa92f33071603dde7882e2968b8f31f6f7d05b36
gpeterso <gpeterso@umn.edu>
2020-03-19 08:28:49 -0500

    Update covid-19 fine notice
    Feedback:
    
      - BF wants to explicitly mention COVID-19 in the notice.
      - Morris does not want to give the impression that they cannot
        accept returns.

--- js/components/account/overview/fines-overview/prm-fines-overview-after.component.js
+++ js/components/account/overview/fines-overview/prm-fines-overview-after.component.js
@@ -1,5 +1,20 @@
 import template from './prm-fines-overview-after.html';
 
+class PrmFinesOverviewAfterController {
+  constructor() {
+    this.mentionReturns = true;
+  }
+
+  $onInit() {
+    if (this.parentCtrl.configUtil.vid === 'MORRIS') {
+      this.mentionReturns = false;
+    }
+  }
+
+}
+
 export default {
-  template: template
+  template: template,
+  bindings: {parentCtrl: '<'},
+  controller: PrmFinesOverviewAfterController
 };
