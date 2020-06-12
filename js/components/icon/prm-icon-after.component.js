105120c59d78cfd0baa356740737529a99f7ba2b
gpeterso <gpeterso@umn.edu>
2020-06-12 17:06:38 -0500

    Override prm-icon template
    In some cases (i.e. in the top bar menu), the pin icon could not
    by dynamically change due to a combination of hard-coded
    hard-coded propertied and one-time binding. It seem like the
    simplest solution is to override the prm-icon template with a
    slightly modified prm-icon-after template.

--- js/components/icon/prm-icon-after.component.js
+++ js/components/icon/prm-icon-after.component.js
@@ -1,21 +1,31 @@
-class PrmIconAfter {
+import template from './icon.html';
 
+/**
+ * Replace the "pin" icon with an MD star icon. Note that we're also overriding
+ * the OTB template here. The reason is that, in some cases (notably the top bar),
+ * the SVG definition is a hard-coded attribute value on the component, and due
+ * to the use of one-time binding, this cannot be dynamically modified. 
+ */
+class PrmIconAfterController {
   $onInit() {
-    //TODO: update aria label properties (in BO???)
-
-    if (this.parentCtrl.iconDefinition === 'prm_pin') {
-      this.parentCtrl.svgIconSet = 'toggle';
-      this.parentCtrl.iconDefinition = 'ic_star_border_24px';
+    switch (this.parentCtrl.iconDefinition) {
+      case 'prm_pin':
+        this.svgIconSet = 'toggle';
+        this.iconDefinition = 'ic_star_border_24px';
+        break;
+      case 'prm_unpin':
+        this.svgIconSet = 'toggle';
+        this.iconDefinition = 'ic_star_24px';
+        break;
+      default:
+        this.svgIconSet = this.parentCtrl.svgIconSet;
+        this.iconDefinition = this.parentCtrl.iconDefinition;
     }
-    if (this.parentCtrl.iconDefinition === 'prm_unpin') {
-      this.parentCtrl.svgIconSet = 'toggle';
-      this.parentCtrl.iconDefinition = 'ic_star_24px';
-    }
-
   }
 }
 
 export default {
-  controller: PrmIconAfter,
+  controller: PrmIconAfterController,
+  template: template,
   bindings: { parentCtrl: '<' },
 };
