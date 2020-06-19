f496aab58312665c589d5a6bb021cc805b679757
gpeterso <gpeterso@umn.edu>
2020-06-19 07:50:26 -0500

    Fix ES Lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "node": true, 
-        "jasmine": true
-    },
-    "extends": ["eslint:recommended",
-                "plugin:protractor/recommended"],
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2,
-            { "SwitchCase": 1 }
-        ],
-        "indent": ["error", 2, { "VariableDeclarator": { "var": 2, "let": 2, "const": 3 } }],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "angular/controller_name": 0
-    },
-    "globals": {
-        "angular": false
-    },
-    "plugins": [
-        "jasmine", 
-        "protractor",
-        "angular"
-    ]
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jasmine: true,
+  },
+  extends: ['eslint:recommended', 'plugin:protractor/recommended'],
+  parserOptions: {
+    sourceType: 'module',
+  },
+  rules: {
+    indent: [
+      'error',
+      2,
+      { VariableDeclarator: { var: 2, let: 2, const: 3 }, SwitchCase: 1 },
+    ],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'angular/controller_name': 0,
+  },
+  globals: {
+    angular: false,
+  },
+  plugins: ['jasmine', 'protractor', 'angular'],
 };
