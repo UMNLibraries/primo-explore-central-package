2c9798610b575d065bdd3402b0351ff4e55309c9
gpeterso <gpeterso@umn.edu>
2020-07-21 16:10:53 -0500

    Add view-level 'no results' component

---
+++ js/components/search/search-result/search-result-list/prm-no-search-result-after.component.js
@@ -0,0 +1,6 @@
+// Add an "after after" directive to allow for view-level customization
+import template from './prm-no-search-result-after.html';
+
+export default {
+  template: template
+};
--- js/components/search/search-result/search-result.module.js
+++ js/components/search/search-result/search-result.module.js
@@ -2,6 +2,7 @@ import HathiTrustAvailability from 'primo-explore-hathitrust-availability';
 import BrowzineService from './search-result-availability/browzine.service';
 import BrowzineComponent from './search-result-availability/browzine.component';
 import PrmSearchResultListAfter from './search-result-list/prm-search-result-list-after.component';
+import PrmNoSearchResultAfter from './search-result-list/prm-no-search-result-after.component';
 import PrmSearchResultAvailabilityLineAfter from './search-result-availability/prm-search-result-availability-line-after.component';
 
 export default angular
@@ -10,6 +11,7 @@ export default angular
   ])
   .service('browzineService', BrowzineService)
   .component('browzine', BrowzineComponent)
+  .component('prmNoSearchResultAfter', PrmNoSearchResultAfter)
   .component('prmSearchResultListAfter', PrmSearchResultListAfter)
   .component('prmSearchResultAvailabilityLineAfter', PrmSearchResultAvailabilityLineAfter)
   .run(['browzineService', browzineService => browzineService.init()])
