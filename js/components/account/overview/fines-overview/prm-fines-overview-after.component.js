dc52fd63a574173737e00195e76c4f52513b9867
gpeterso <gpeterso@umn.edu>
2020-07-22 11:09:13 -0500

    Update COVID-19 fines/fees msg
    Closes #16

--- js/components/account/overview/fines-overview/prm-fines-overview-after.component.js
+++ js/components/account/overview/fines-overview/prm-fines-overview-after.component.js
@@ -1,20 +1,6 @@
 import template from './prm-fines-overview-after.html';
 
-class PrmFinesOverviewAfterController {
-  constructor() {
-    this.mentionReturns = true;
-  }
-
-  $onInit() {
-    if (this.parentCtrl.configUtil.vid === 'MORRIS') {
-      this.mentionReturns = false;
-    }
-  }
-
-}
-
 export default {
   template: template,
-  bindings: {parentCtrl: '<'},
-  controller: PrmFinesOverviewAfterController
+  bindings: { parentCtrl: '<' },
 };
