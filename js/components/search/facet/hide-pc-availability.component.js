d42994b4cf22b9764f6d0d53723e6add8685b4c1
gpeterso <gpeterso@umn.edu>
2020-10-13 14:25:23 -0500

    Show 'expand' facet for MORRIS|CROOKSTON
    Display the "expand beyond" facet in the MORRIS & CROOKSTON views.
    It's likely that all campuses will soon follow suit, at which point
    this component can be removed.

--- js/components/search/facet/hide-pc-availability.component.js
+++ js/components/search/facet/hide-pc-availability.component.js
@@ -1,20 +1,36 @@
+const exceptions = new Set(['MORRIS', 'CROOKSTON']);
+
+/**
+ * hide the "expand beyond" facet, unless:
+ *  1) the campus is an `exception`, or
+ *  2) the URL includes a 'showExpand' parameter
+ */
 class HidePcAvailabilityController {
-  constructor($location) {
+  constructor($location, $window) {
     this.$location = $location;
+    this.institution =
+      $window.appConfig['primo-view']['institution']['institution-code'];
   }
 
   $onInit() {
-    if (this.$location.search()['showExpand'] !== 'true') {
+    console.log('hasInst: ', exceptions.entries());
+    if (exceptions.has(this.institution) || this.overridden) {
+      this.prmFacetCtrl.showPcAvailability = true;
+    } else {
       this.prmFacetCtrl.showPcAvailability = false;
     }
+  }
+
+  get overridden() {
+    return this.$location.search()['showExpand'] == true;
   }
 }
 
 let HidePcAvailability = {
-  require: {prmFacetCtrl: '^prmFacet'},
-  controller: HidePcAvailabilityController
+  require: { prmFacetCtrl: '^prmFacet' },
+  controller: HidePcAvailabilityController,
 };
 
-HidePcAvailability.$inject = ['$location'];
+HidePcAvailability.$inject = ['$location', '$window'];
 
 export default HidePcAvailability;
