c327767863381ade0287715a777f086656b70f75
gpeterso <gpeterso@umn.edu>
2021-01-08 16:03:50 -0600

    Revert "Add Duluth GetIt note"
    This reverts commit 85d41114869c078d8ba594029e156088211f4629.

--- js/components/search/full-view/full-view-service-container/change-alma-skin.component.js
+++ js/components/search/full-view/full-view-service-container/change-alma-skin.component.js
@@ -3,10 +3,7 @@ const NEW_PARAM = '&req.skin=umn_nui';
 
 class ChangeAlmaSkinController {
   $onInit() {
-    if (
-      this.hasLinkElement() &&
-      this.prmFullViewServiceContainerCtrl.isMashupLink()
-    ) {
+    if (this.hasLinkElement() && this.prmFullViewServiceContainerCtrl.isMashupLink()) {
       let oldUrl = this.link;
       if (!oldUrl.includes(NEW_PARAM)) {
         this.link = oldUrl.replace(OLD_PARAM, NEW_PARAM);
@@ -15,8 +12,7 @@ class ChangeAlmaSkinController {
   }
 
   get link() {
-    return this.prmFullViewServiceContainerCtrl.service.linkElement.links[0]
-      .link;
+    return this.prmFullViewServiceContainerCtrl.service.linkElement.links[0].link;
   }
 
   set link(val) {
@@ -26,11 +22,12 @@ class ChangeAlmaSkinController {
   hasLinkElement() {
     return !!this.prmFullViewServiceContainerCtrl.service.linkElement;
   }
+
 }
 
 let ChangeAlmaSkin = {
-  require: { prmFullViewServiceContainerCtrl: '^prmFullViewServiceContainer' },
-  controller: ChangeAlmaSkinController,
+  require: {prmFullViewServiceContainerCtrl: '^prmFullViewServiceContainer'},
+  controller: ChangeAlmaSkinController
 };
 
 export default ChangeAlmaSkin;
--- js/components/search/full-view/full-view-service-container/full-view-service-container.module.js
+++ js/components/search/full-view/full-view-service-container/full-view-service-container.module.js
@@ -1,10 +1,8 @@
 import ChangeAlmaSkin from './change-alma-skin.component';
-import GetItNote from './get-it-note.component';
 import PrmFullViewServiceContainerAfter from './prm-full-view-service-container-after.component';
 
 export default angular
   .module('fullViewServiceContainer', [])
   .component('changeAlmaSkin', ChangeAlmaSkin)
-  .component('getItNote', GetItNote)
   .component('prmFullViewServiceContainerAfter', PrmFullViewServiceContainerAfter)
   .name;
