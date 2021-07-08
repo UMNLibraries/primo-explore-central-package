11919046a932ff05616ef8a35baae6448a274063
gpeterso <gpeterso@umn.edu>
2021-07-08 12:55:27 -0500

    Update the custom tiles component
    - use the Config service instead of directly accessing `window.appConfig`
    - use a dynamic template string instead of a static HTML file
    - conditionally display the new ILLidad integration components
    
    Resolves: #11

--- js/components/account/overview/custom-tiles/inject-custom-tiles.component.js
+++ js/components/account/overview/custom-tiles/inject-custom-tiles.component.js
@@ -1,47 +1,64 @@
-import template from './custom-tiles.html';
-
 /**
- * Appends the "custom-tiles" template to the My Account Overview grid. 
- * 
+ * Appends the "custom-tiles" template to the My Account Overview grid.
+ *
  * This component (element directive) should be placed in any of the
- * prm-{tile}-overview-after components as a child element. For example: 
+ * prm-{tile}-overview-after components as a child element. For example:
  *  <prm-loans-overview-after>
  *    <custom-tiles></custom-tiles>
  *  </prm-loans-overview-after>
- * 
+ *
  */
 class InjectCustomTilesController {
-  constructor($window, $scope, $compile, $element) {
+  constructor($window, $scope, $compile, $element, config) {
     this.$scope = $scope;
     this.$element = $element;
-    this.template = $compile(template)($scope);
-    this.institution = 
-      $window.appConfig['primo-view']['institution']['institution-code'];
+    this.$compile = $compile;
+    this.config = config;
   }
 
-  getAccountOverviewGrid(){
+  getAccountOverviewGrid() {
     return this.$element.parent().parent().parent();
   }
 
   appendCustomTiles() {
-    let unbindWatch = this.$scope.$watch(() => this.getAccountOverviewGrid(), 
-      accountOverviewGrid => {
-        if (accountOverviewGrid) {
+    const unbindWatch = this.$scope.$watch(
+      () => this.getAccountOverviewGrid(),
+      (accountOverviewGrid) => {
+        if (accountOverviewGrid.hasOwnProperty('length')) {
           accountOverviewGrid.append(this.template);
           unbindWatch();
-        } 
+        }
       }
     );
   }
 
+  get enableIlliad() {
+    return this.config.enableIlliad;
+  }
+
+  get template() {
+    // show either the ILLiad-integration components or the generic ILL component.
+    let html = this.enableIlliad
+      ? '<ill-requests></ill-requests><ill-articles></ill-articles>'
+      : '<ill></ill>';
+    html += '<courses></courses>';
+    return this.$compile(html)(this.$scope);
+  }
+
   $postLink() {
-    if (this.institution == 'TWINCITIES') {
+    if (this.config.showCustomAccountTiles) {
       this.appendCustomTiles();
     }
   }
 }
 
-InjectCustomTilesController.$inject = ['$window', '$scope', '$compile', '$element'];
+InjectCustomTilesController.$inject = [
+  '$window',
+  '$scope',
+  '$compile',
+  '$element',
+  'config',
+];
 
 export default {
   controller: InjectCustomTilesController,
